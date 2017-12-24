f60ceb6346d0a72c1c49c54d1d163bd0bac079cd
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2017-12-24 20:30:41 +0900

    Added example codes to fetch daily step sources.

--- Example/HealthStore/ViewController.swift
+++ Example/HealthStore/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import HealthKit
+import HealthStore
 
 class ViewController: UIViewController {
+
+    let healthStore = HealthStore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let builder = DailyQueryBuilder(quantityType: type)
+        healthStore?.observeSourceUpdates(from: Date(), builder: builder, handler: { [weak self] (_: [StepSource]?) in
+            self?.reloadSteps()
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func reloadSteps() {
+        guard let healthStore = healthStore else { return }
+        let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let builder = DailyQueryBuilder(quantityType: type)
+        healthStore.requestSources(from: Date.startOfToday(), to: Date(), builder: builder) { (sources: [StepSource]?) in
+            if let sources = sources {
+                print("sources: \(sources)")
+            } else {
+                print("step sources are not found.")
+            }
+        }
     }
-
 }
-
